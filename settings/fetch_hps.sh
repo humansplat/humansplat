c93c45fba35e8372e1cd83b70e3c1d8ba4bb516f
panpanwang <panpanwang@bytedance.com>
2024-12-10 11:44:30 +0800

    [update] update readme

--- settings/fetch_hps.sh
+++ settings/fetch_hps.sh
@@ -5,17 +5,27 @@ urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i
 
 
 PROJ_DIR=$(pwd)
-DOWNLOAD_PATH="extensions/extensions/pixielib/HPS/pixie_data"
-
+DOWNLOAD_PATH="extensions/pixielib/HPS/pixie_data"
+mkdir -p ${DOWNLOAD_PATH}
 echo -e "\nYou need to login https://pixie.is.tue.mpg.de/ &  https://smpl-x.is.tue.mpg.de/, and register SMPL-X and PIXIE"
 read -p "Username (SMPL-X):" username
 read -p "Password (SMPL-X):" password
+
 username=$(urle $username)
 password=$(urle $password)
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' -O ${DOWNLOAD_PATH}/SMPLX_NEUTRAL_2020.npz --no-check-certificate --continue
 
+# SMPLX Models
+{
+    wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' -O ${DOWNLOAD_PATH}/SMPLX_NEUTRAL_2020.npz --no-check-certificate --continue
+} || {
+    wget https://huggingface.co/lilpotat/pytorch3d/resolve/346374a95673795896e94398d65700cb19199e31/SMPLX_NEUTRAL_2020.npz -O ${DOWNLOAD_PATH}/SMPLX_NEUTRAL_2020.npz  --continue
+}
 # PIXIE pretrained model and utilities
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=pixie&sfile=pixie_model.tar&resume=1' -O ${DOWNLOAD_PATH}/pixie_model.tar --no-check-certificate --continue
+{
+    wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=pixie&sfile=pixie_model.tar&resume=1' -O ${DOWNLOAD_PATH}/pixie_model.tar --no-check-certificate --continue
+} || {
+    wget https://huggingface.co/lilpotat/pytorch3d/resolve/main/pixie_model.tar -O ${DOWNLOAD_PATH}/pixie_model.tar  --continue
+}
 cd ${DOWNLOAD_PATH} && tar xvf utilities.tar
 cd $PROJ_DIR
 
--- settings/setup.sh
+++ settings/setup.sh
@@ -7,10 +7,20 @@ pip3 install -r requirements.txt
 if [ ! -d "extensions/diff-gaussian-rasterization" ]; then
     cd extensions && git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization 
 fi
+
+# install gaussian-rasterization
 cd $PROJ_DIR && pip3 install ./extensions/diff-gaussian-rasterization
 
-
-
+# install pytorch3d
+{
+    python3 extensions/install_pytorch3d.py
+} || {
+    export MAKEFLAGS="-j32" pip3 install "git+https://github.com/facebookresearch/pytorch3d.git@stable" 
+}
 
 # download pre-trained ckpts
+
+
+
+
 
